67f341e827b8695d92419d62b967529b7d3e17a5
artor <artor.dev@gmail.com>
2022-06-16 11:14:28 +0200

    fix wo not work: jwt in kebab

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,4 @@
+// node -e "console.log(require('crypto').randomBytes(16).toString('base64'));"
 module.exports = ({ env }) => ({
-    jwtSecret: 'jdiozs86d78678sdu'
+    jwtSecret: 'a-a-a-a-a'
 });
